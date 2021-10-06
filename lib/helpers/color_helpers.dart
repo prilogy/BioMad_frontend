1b76ac9e85ab5a23caef5a03172655a6eb2b0130
Artyom Lukyanov <artglz63@gmail.com>
2021-10-06 15:45:16 +0300

    fixed radix

--- lib/helpers/color_helpers.dart
+++ lib/helpers/color_helpers.dart
@@ -3,10 +3,15 @@ import 'package:flutter/material.dart';
 class ColorHelpers {
   /// String is in the format "aabbcc" or "ffaabbcc" with an optional leading "#".
   static Color fromHex(String hexString) {
-    final buffer = StringBuffer();
-    if (hexString.length == 6 || hexString.length == 7) buffer.write('ff');
-    buffer.write(hexString.replaceFirst('#', ''));
-    return Color(int.parse(buffer.toString(), radix: 16));
+    try {
+      final buffer = StringBuffer();
+      if (hexString.length == 6 || hexString.length == 7) buffer.write('ff');
+      buffer.write(hexString.replaceFirst('#', ''));
+      return Color(int.parse(buffer.toString(), radix: 16));
+    }
+    catch(e) {
+      return Colors.blueAccent;
+    }
   }
 
   /// Prefixes a hash sign if [leadingHashSign] is set to `true` (default is `true`).
